2b2b90a98e4bd2b1e1b4eafe17f6a7f6a276e986
kazu-0226 <59648368+kazu-0226@users.noreply.github.com>
2022-02-17 12:49:15 +0900

    memoで最適化

--- src/App.js
+++ src/App.js
@@ -4,13 +4,8 @@ import "./styles.css";
 import { ChildArea } from "./ChildArea";
 
 export default function App() {
-  // const [count, setCount] = useState(0);
   const [text, setText] = useState("");
   const [open, setOpen] = useState(false);
-
-  // const onClickCountUp = () => {
-  //   setCount(count + 1);
-  // };
 
   const onChangeText = (e) => {
     console.log(e.target.value);
@@ -22,11 +17,6 @@ export default function App() {
   return (
     <>
       <div className="App">
-        {/* <div>
-          <p>カウント</p>
-          <p>{count}</p>
-          <button onClick={onClickCountUp}>カウントアップ</button>
-        </div> */}
         <div>
           <input value={text} onChange={onChangeText} />
           <br />
