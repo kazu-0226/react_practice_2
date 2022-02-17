ec7f2dac325bbebb4163ccdc86d0a8f46e0591dc
kazu-0226 <59648368+kazu-0226@users.noreply.github.com>
2022-02-17 11:08:44 +0900

    useState確認

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import { useState } from "react";
 import "./styles.css";
 
+import { ChildArea } from "./ChildArea";
+
 export default function App() {
-  return <div className="App">Hello!World!!</div>;
+  // const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  // const onClickCountUp = () => {
+  //   setCount(count + 1);
+  // };
+
+  const onChangeText = (e) => {
+    console.log(e.target.value);
+    setText(e.target.value);
+  };
+
+  const onClickOpen = () => setOpen(!open);
+
+  return (
+    <>
+      <div className="App">
+        {/* <div>
+          <p>カウント</p>
+          <p>{count}</p>
+          <button onClick={onClickCountUp}>カウントアップ</button>
+        </div> */}
+        <div>
+          <input value={text} onChange={onChangeText} />
+          <br />
+          <br />
+          <button onClick={onClickOpen}>表示</button>
+          <ChildArea open={open} />
+        </div>
+      </div>
+    </>
+  );
 }
